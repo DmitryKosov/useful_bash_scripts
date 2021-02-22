9ee84a320799c738dd2976ef183da194336be71c
Dmitry Kosov <dimman@localhost.localdomain>
2021-02-22 13:56:34 +0100

    update vim script

--- vim_yaml.sh
+++ vim_yaml.sh
@@ -10,10 +10,10 @@ set number
 EOF
 
 # Install yamllint python package. Right now just för debian-based distros
-sudo apt install yamllint
+sudo dnf install yamllint
 
-mkdir ~/config
-mkdir ~/.config/yamllint
+
+mkdir -p ~/.config/yamllint
 
 cat >>~/.config/yamllint/config<<EOF
 extends: relaxed
@@ -33,3 +33,16 @@ let g:ale_sign_error = '✘'
 let g:ale_sign_warning = '⚠'
 let g:ale_lint_on_text_changed = 'never'
 EOF
+# TODO make system agnostic
+#Powerline
+sudo dnf install vim-plugin-powerline
+
+cat >>.vimrc<<EOF	
+" Powerline "
+
+python from powerline.vim import setup as powerline_setup
+python powerline_setup()
+python del powerline_setup
+set laststatus=2         " Always show statusline
+EOF
+
